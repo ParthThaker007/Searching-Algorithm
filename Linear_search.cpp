2ca2cff540f8450bceb7c0475159c40c5b290070
ParthThaker007 <101121538+ParthThaker007@users.noreply.github.com>
2023-06-22 11:09:15 +0530

    Add files via upload

---
+++ Linear_search.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+	int SearchElement, Size;
+	cout << "Enter the number of items:" << "\n";
+	cin >>Size;
+	int *arr = new int(Size);
+	cout << "Enter the elements of array" << endl;
+	for (int i= 0; i < Size; i++) {
+		cin >> arr[i];
+	}
+	
+
+    cout<<"Enter the element to be searched"<<endl;
+    cin>>SearchElement;
+    for(int i=0;i<Size;i++){
+        if(arr[i]==SearchElement){
+            cout<<"Element Found at "<<i<<" Position"<<endl;
+        }
+    }
+    return 0;
+}
